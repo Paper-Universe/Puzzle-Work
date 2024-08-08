b6f2ff6d20166b492bcca446dd9262319d143cb4
Paper-Universe <165382418+Paper-Universe@users.noreply.github.com>
2024-08-07 19:10:21 -0700

    Update HexGen.py

--- HexGen.py
+++ HexGen.py
@@ -6,7 +6,7 @@ count = 0
 spaces = 0
 range2 = 18 # Amount of total digits in hex range
 file2 = open('list2.txt','a')
-#268435455
+#16777215
 st = time()
 with open('list2.txt', 'r') as file3:
         for line in file3:
@@ -14,24 +14,25 @@ with open('list2.txt', 'r') as file3:
 list = []
 file = open("list2.txt", 'r') # Make an empty list.txt file before you start program
 randomList = file.read()
-total = (16777215 - spaces)
+total = (255)
 print(total)
 while count in range(total):
-    rand = random.randint(0x1000000, 0x1ffffff) # currently 6 0's and f's or 44 bit
+    rand = random.randint(0x1ffff00, 0x1ffffff) # currently 6 0's and f's or 44 bit
     hexify = hex(rand)
     stripped = hexify[2:]
-    file.read()
-    if stripped not in list or randomList:
+    file = open("list2.txt", 'r')
+    randomList = file.read()
+    if stripped not in randomList or list:
         list.append(stripped)
+        if count % 1 == 0: 
+            print(f'[+] Completed: {count}', end='\r')
+            file2.write(('{} '*len(list)).format(*list))
+            list.clear()
         count += 1
         file2.flush()
         os.fsync(file2.fileno())
     elif stripped in list or randomList:
         continue
-    if count % 1000 == 0: 
-        print(f'[+] Completed: {count}', end='\r')
-        file2.write(('{} '*len(list)).format(*list))
-        list.clear()
 file2.close()
 print(f'[+] Finished Total Keys: {total}\n')
 print(f'[+] Completed in {time() - st:.2f} sec')
