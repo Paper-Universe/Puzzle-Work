ece781f7bb4a02007bbff982a61b3cb8285d66a4
Paper-Universe <165382418+Paper-Universe@users.noreply.github.com>
2024-08-07 18:23:50 -0700

    Update HexGen.py

--- HexGen.py
+++ HexGen.py
@@ -5,22 +5,26 @@ from time import time
 count = 0
 spaces = 0
 range2 = 18 # Amount of total digits in hex range
-total = 268435455 # Max Range in Decimal. Max Range = (end range - start)
-#268435455
 file2 = open('list2.txt','a')
+#268435455
 st = time()
-
+with open('list2.txt', 'r') as file3:
+        for line in file3:
+            spaces += line.count(' ')
 list = []
 file = open("list2.txt", 'r') # Make an empty list.txt file before you start program
 randomList = file.read()
-
+total = (268435455 - spaces)
+print(total)
 while count in range(total):
     rand = random.randint(0x10000000, 0x1fffffff) # currently 7 0's and f's or 40 bit
     hexify = hex(rand)
     stripped = hexify[2:]
-    count += 1
+    file = open("list2.txt", 'r')
+    randomList = file.read()
     if stripped not in list or randomList:
         list.append(stripped)
+        count += 1
         file2.flush()
         os.fsync(file2.fileno())
     elif stripped in list or randomList:
