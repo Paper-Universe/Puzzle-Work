6ada5a904c5ed3eb596b57345de874db0efc7b11
Paper-Universe <165382418+Paper-Universe@users.noreply.github.com>
2024-10-04 10:06:52 -0700

    Update and rename Pubkey_stride.py to Pubkey_search.py

--- Pubkey_search.py
+++ Pubkey_search.py
@@ -7,13 +7,12 @@ rangestart = '100000000000000000'
 rangeend = '1fffffffffffffffff'
 i = 0
 count = 0
-stride = 0xfffffffffff
 range2 = 0xfffffffffffffffff
 list = []
 P = '04608c7b8f41eae057b976053cbf001346e353b3b8aab6b7cd1ed689f35889ac7a20bd5531eb7da2dbcd5277a43655add6e69a9c0ffceae396484cb35f6d47eee8'
 scrambles = binascii.unhexlify(P)
 list.append(scrambles)
-print('Expected:', int(range2 / stride))
+print('Expected:', int(range2)
 def world():
     publickey_inc = ice.point_increment(list[0])
     list.clear()
@@ -34,7 +33,7 @@ def world():
 while i in range(0xfffffffffffffffff):
     try:
         public, match = world()
-        i += stride
+        i += 1
         count += 1
         #print(match)
         print(f'[+] Completed: {count}', end='\r')
@@ -46,9 +45,3 @@ while i in range(0xfffffffffffffffff):
 print('  Program Finished  ')
 
 #start P = 0485672c7d2de0b7da2bd1770d89665868741b3f9af7643397721d74d28134ab837c481b9b5b43b2eb6374049bfa62c2e5e77f17fcc5298f44c8e3094f790313a6
-#adding stride can't check through the public key if it goes over so we will need to check through our i
-'''
-we just need to add to i what we put into stride so if stride = 10 then i += 1
-also add a ctrl exit like in seed puzzle from iceland and make it print out the last public key it hashed so we can save where we ended
-'''
-#look at icelands and others code to see how they get 3.5 million keys per second
